9195b632425434517626755d683ad1af541549d1
Strider <dave.barnes23@gmail.com>
2022-06-10 18:39:46 -0500

    api's functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,63 @@
+var searchForm = document.querySelector("#searchForm");
+var searchInput = document.querySelector("#searchInput");
 var searchBtn = document.querySelector("#searchBtn");
 var searchHistory = document.querySelector("#searchHistory");
 var cityResults = document.querySelector("#cityResults");
 var cityName = document.querySelector("#cityName");
 var foreCast = document.querySelector("#foreCast");
 // debugger;
-var getLocationInfo = function(){
+var searchSubmit = function(event){
+    // debugger;
+    event.preventDefault();
+    var citySearch = searchInput.value.trim();
+    console.log(citySearch);
+
+    if (citySearch) {
+        searchLatLon(citySearch);
+        // console.log(citySearch);
+        // searchInput.value = "";
+    } else {
+        alert("Please enter: city name, state code(ex. TX), country code(ex. US)")
+    }
+};
+
+var searchLatLon = function(city){
+    console.log(city);
+    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=f64460980741f108f643fa3eeb49f4b2";
+
+    fetch(apiUrl).then(function(response) {
+
+        // request was successful
+        if (response.ok) {
+          response.json().then(function(data) {
+          console.log(data);
+          console.log(data[0].lat); 
+          console.log(data[0].lon);
+          getLocationInfo(data);
+        });
+    }
+    // else {
+    //   console.log(response);
+    //   alert("There was a problem with your request!");
+    // }
+  }); 
+};
+
+var getLocationInfo = function(data){
     // openweather API url
-    var apiUrl = "https:api.openweathermap.org/data/2.5/onecall?lat=" + 47.6062 + "&lon=" + -122.3321 + "&units=imperial&exclude=hourly,minutely&appid=f64460980741f108f643fa3eeb49f4b2";
+    var apiUrl = "https:api.openweathermap.org/data/2.5/onecall?lat=" + data[0].lat + "&lon=" + data[0].lon + "&units=imperial&exclude=hourly,minutely&appid=f64460980741f108f643fa3eeb49f4b2";
     
     fetch(apiUrl).then(function(response) {
 
         // request was successful
         if (response.ok) {
           response.json().then(function(data) {
-          console.log(data);  
-          displayResults(data);
+          console.log(data);
+          console.log(data.current.humidity);  
+          console.log(data.current.temp);
+          console.log(data.current.wind_speed);
+          console.log(data.current.uvi);
+        //   displayResults(data);
           });
         }
         else {
@@ -25,5 +68,7 @@ var getLocationInfo = function(){
 };
 
 
+searchForm.addEventListener("submit", searchSubmit);
 
-getLocationInfo();
+// searchLatLon();
+// getLocationInfo();
