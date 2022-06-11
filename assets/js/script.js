6eece825589abbbbdecd5ff86983e6640d4d335f
Strider <dave.barnes23@gmail.com>
2022-06-11 13:26:14 -0500

    api fetch complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
+var date = new Date()
+console.log(date);
+var day = date.getDate();
+console.log(day);
+var month = date.getMonth()+1;
+console.log(month);
+var year = date.getFullYear();
+console.log(year);
+
 var searchForm = document.querySelector("#searchForm");
 var searchInput = document.querySelector("#searchInput");
 var searchBtn = document.querySelector("#searchBtn");
@@ -16,34 +25,42 @@ var searchSubmit = function(event){
         searchLatLon(citySearch);
         // console.log(citySearch);
         // searchInput.value = "";
-    } else {
-        alert("Please enter: city name, state code(ex. TX), country code(ex. US)")
-    }
+    } 
+    // else {
+    //     alert("Please enter: city name, state code(ex. TX), country code(ex. US)")
+    // }
 };
 
+// fetch API for getting latitude and longitude
 var searchLatLon = function(city){
     console.log(city);
+
     var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=f64460980741f108f643fa3eeb49f4b2";
 
     fetch(apiUrl).then(function(response) {
 
         // request was successful
         if (response.ok) {
-          response.json().then(function(data) {
-          console.log(data);
-          console.log(data[0].lat); 
-          console.log(data[0].lon);
-          getLocationInfo(data);
-        });
-    }
-    // else {
-    //   console.log(response);
-    //   alert("There was a problem with your request!");
-    // }
-  }); 
+            response.json().then(function(data) {
+            console.log(data);
+            console.log(data[0].name);
+            console.log(data[0].lat); 
+            console.log(data[0].lon);
+            // displayResults(data);
+            getLocationInfo(data);
+            });
+        } 
+        // else {
+        // console.log(response);
+        // alert("There was a problem with your request!");
+        // }
+    }); 
 };
 
+// fetch API to pull weather data for searched location
 var getLocationInfo = function(data){
+    // create textContent City Name
+    cityName.textContent = data[0].name + " (" + month + "/" + day + "/" + year + ") ";
     // openweather API url
     var apiUrl = "https:api.openweathermap.org/data/2.5/onecall?lat=" + data[0].lat + "&lon=" + data[0].lon + "&units=imperial&exclude=hourly,minutely&appid=f64460980741f108f643fa3eeb49f4b2";
     
@@ -66,6 +83,12 @@ var getLocationInfo = function(data){
         }
       });
 };
+
+var displayResults = function(data) {
+    cityResults.textContent
+    var cityInfoList = document.createElement("ul")
+    
+}
 
 
 searchForm.addEventListener("submit", searchSubmit);
