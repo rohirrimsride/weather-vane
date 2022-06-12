936258c109467ecaea607a21a5a60193ad95ed4c
Strider <dave.barnes23@gmail.com>
2022-06-11 21:35:57 -0500

    working on creating cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,9 @@ var month = date.getMonth()+1;
 console.log(month);
 var year = date.getFullYear();
 console.log(year);
+var currentDate = " (" + month + "/" + day + "/" + year + ") ";
+console.log(currentDate)+1;
+// figure out how to add days to currentDate
 
 var searchForm = document.querySelector("#searchForm");
 var searchInput = document.querySelector("#searchInput");
@@ -35,19 +38,19 @@ var searchSubmit = function(event){
 var searchLatLon = function(city){
     console.log(city);
 
-    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=f64460980741f108f643fa3eeb49f4b2";
+    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=f64460980741f108f643fa3eeb49f4b2";
 
     fetch(apiUrl).then(function(response) {
 
         // request was successful
         if (response.ok) {
-            response.json().then(function(data) {
-            console.log(data);
-            console.log(data[0].name);
-            console.log(data[0].lat); 
-            console.log(data[0].lon);
+            response.json().then(function(cityData) {
+            console.log(cityData);
+            console.log(cityData[0].name);
+            console.log(cityData[0].lat); 
+            console.log(cityData[0].lon);
             // displayResults(data);
-            getLocationInfo(data);
+            getLocationInfo(cityData);
             });
         } 
         // else {
@@ -58,15 +61,10 @@ var searchLatLon = function(city){
 };
 
 // fetch API to pull weather data for searched location
-var getLocationInfo = function(data){
-    // create textContent City Name
-    cityName.textContent = data[0].name + " (" + month + "/" + day + "/" + year + ") ";
-        if (data[0].clouds >= 66) {
-            var skyCondition = document.createElement("i");
-            skyCondition
-        }
+var getLocationInfo = function(cityData){
+        
     // openweather API url
-    var apiUrl = "https:api.openweathermap.org/data/2.5/onecall?lat=" + data[0].lat + "&lon=" + data[0].lon + "&units=imperial&exclude=hourly,minutely&appid=f64460980741f108f643fa3eeb49f4b2";
+    var apiUrl = "https:api.openweathermap.org/data/2.5/onecall?lat=" + cityData[0].lat + "&lon=" + cityData[0].lon + "&units=imperial&exclude=hourly,minutely&appid=f64460980741f108f643fa3eeb49f4b2";
     
     fetch(apiUrl).then(function(response) {
 
@@ -78,13 +76,15 @@ var getLocationInfo = function(data){
             console.log(data.current.temp);
             console.log(data.current.wind_speed);
             console.log(data.current.uvi);
-            displayResults(data);
+            displayResults(data, cityData);
           });
         }
     });
 };
 
-var displayResults = function(data) {
+var displayResults = function(data, cityData) {
+    // create textContent City Name
+    cityName.textContent = cityData[0].name + currentDate + data.current.weather.icon;
 
     var cityTemp = document.querySelector("#cityTemp");
     cityTemp.textContent = data.current.temp;
@@ -120,6 +120,13 @@ var displayResults = function(data) {
             cityUvi.className = ("uvi-extreme");
 
         };
+
+    var dayOneDate = document.querySelector("#dayOneDate");
+    dayOneDate.textContent = data.daily[0].dt;
+    console.log(data.daily[0].dt);
+// finish up cards
+    
+
 
 };
 
